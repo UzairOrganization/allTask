788554f49f503c036603f9af8f051c2c870ccae9
hamzahashmi033 <hashmihamza846@gmail.com>
2025-11-03 19:31:44 +0500

    push

--- src/lib/data-service.js
+++ src/lib/data-service.js
@@ -1,6 +1,6 @@
 const NODE_ENV = "production"
 
-export const API = "http://localhost:4001"
+export const API = "https://api.alltasko.com"
 
 import axios from "axios"
 import { notFound } from "next/navigation"
